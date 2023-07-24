a707fa9cd065675e8fe6e762fddc52f60730aeed
manju summoner <57256282+manju-summoner@users.noreply.github.com>
2023-07-24 23:37:23 +0900

    Create CloseXIcon.user.js

---
+++ CloseXIcon.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Close X Icon
+// @namespace    https://github.com/manju-summoner/Close-X-Icon
+// @version      0.1
+// @description  Now you can close X by clicking on the X icon!
+// @author       manju_summoner
+// @match        https://twitter.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+(async function() {
+    'use strict';
+
+    // Your code here...
+    var element = null;
+    while(element == null)
+    {
+        element = document.querySelector('[aria-label="Twitter"]');
+        await new Promise(r => setTimeout(r, 1000));
+    }
+    element.onmousedown = function()
+    {
+        window.location.href="about:blank";
+        return false;
+    };
+})();
